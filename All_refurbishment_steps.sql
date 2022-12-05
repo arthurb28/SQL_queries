c706abb3061cd4a6d6b18e12b2e0243b6d31ac70
ArthurBignon1 <103106470+ArthurBignon1@users.noreply.github.com>
2022-12-05 10:48:59 +0100

    Update All_refurbishment_steps.sql

--- All_refurbishment_steps.sql
+++ All_refurbishment_steps.sql
@@ -1,3 +1,4 @@
+-- Environment :RS_BA_RETAIL_PURCHASE_FR
 select
 rt.*
 
@@ -6,5 +7,17 @@ left join wkda_retail_refurbishment.refurbishment as r on r.retail_ad_id = ra.id
 left join wkda_retail_refurbishment.refurbishment_transition rt on rt.refurbishment_id = r.id
 
 where ra.stock_number = 'DW39504'
+
+order by transition_date
+
+-- Environment :RS_BA_RETAIL_PURCHASE_FR_2
+select
+rt.*
+
+from wkda_retail_purchase_fr.retail_ad as ra
+left join wkda_retail_purchase_fr.retail_refurbishment as r on r.retail_ad_id = ra.id
+left join wkda_retail_purchase_fr.retail_refurbishment_transition rt on rt.refurbishment_id = r.id
+
+where ra.stock_number = 'TP26831'
 
 order by transition_date
